62de2a683298ca5dd5437df25f93f122dfcfa694
Oleg Rybakov <rybakovo@amazon.com>
2016-12-19 12:20:25 -0800

    refactor sort test

--- tst/gputests/TestSort.cpp
+++ tst/gputests/TestSort.cpp
@@ -10,247 +10,213 @@
 #include "kernels.h"
 #include "Utils.h"
 
-
-using namespace std;
-
 void randData(NNFloat* pTarget, NNFloat* pOut, const size_t batch, const size_t nFeatures, const size_t stride) {
-  memset(pTarget, 0, stride * batch * sizeof(NNFloat));
-  memset(pOut, 0, stride * batch * sizeof(NNFloat));
-  for (size_t i = 0; i < batch; i++) {
+    memset(pTarget, 0, stride * batch * sizeof(NNFloat));
+    memset(pOut, 0, stride * batch * sizeof(NNFloat));
+    for (size_t i = 0; i < batch; i++) {
 
-    for (size_t k = 0; k < nFeatures; k++) {
-      pTarget[k] = rand(0, nFeatures - 1);
+        for (size_t k = 0; k < nFeatures; k++) {
+            pTarget[k] = rand(0, nFeatures - 1);
+        }
+
+        for (size_t o = 0; o < nFeatures; o++) {
+            pOut[o] = rand(0.f, 1.f);
+        }
+
+        pTarget += stride;
+        pOut += stride;
     }
-
-    for (size_t o = 0; o < nFeatures; o++) {
-      pOut[o] = rand(0.f, 1.f);
-    }
-
-    pTarget += stride;
-    pOut += stride;
-  }
 }
 
 bool testTopK(const size_t batch = 128, const size_t topK = 128, const size_t nFeatures = 1024) {
 
-  cout << "TEST kCalculateTopK with parameters: " << "batch=" << batch << " topK=" << topK << " nFeatures=" << nFeatures << endl;
-  bool ret = true;
+    cout << "TEST kCalculateTopK with parameters: " << "batch=" << batch << " topK=" << topK << " nFeatures=" << nFeatures << endl;
+    bool ret = true;
 
-  const float EPS = 1.e-6;
-  const size_t STRIDE = ((nFeatures + 127) >> 7) << 7;
-  timeval t0, t1;
+    const float eps = 1.e-6;
+    const size_t stride = ((nFeatures + 127) >> 7) << 7;
+    timeval t0, t1;
 
-  // allocate memory same way with main engine
-  GpuBuffer<NNFloat>* pbKey = new GpuBuffer<NNFloat>(batch * topK, true);
-  GpuBuffer<NNFloat>* pbFValue = new GpuBuffer<NNFloat>(batch * topK, true);
-  GpuBuffer<unsigned int>* pbUIValue = new GpuBuffer<unsigned int>(batch * topK, true);
+    std::unique_ptr<GpuBuffer<NNFloat> > pbKey(new GpuBuffer<NNFloat>(batch * topK, true));
+    std::unique_ptr<GpuBuffer<NNFloat> > pbFValue(new GpuBuffer<NNFloat>(batch * topK, true));
+    std::unique_ptr<GpuBuffer<unsigned int> > pbUIValue( new GpuBuffer<unsigned int>(batch * topK, true));
 
-  GpuBuffer<NNFloat>* pbTarget = new GpuBuffer<NNFloat>(batch * STRIDE, true);
-  GpuBuffer<NNFloat>* pbOutput = new GpuBuffer<NNFloat>(batch * STRIDE, true);
+    std::unique_ptr<GpuBuffer<NNFloat> > pbTarget( new GpuBuffer<NNFloat>(batch * stride, true));
+    std::unique_ptr<GpuBuffer<NNFloat> > pbOutput(new GpuBuffer<NNFloat>(batch * stride, true));
 
-  cout << "1 TEST kCalculateTopK with 3 args" << endl;
+    cout << "1 TEST kCalculateTopK with 3 args" << endl;
 
-  // prepare data
-  {
-    NNFloat* pTarget = pbTarget->_pSysData;
-    NNFloat* pOut = pbOutput->_pSysData; // data from output layer (scores)
+    // prepare data
+    {
+        NNFloat* pTarget = pbTarget->_pSysData;
+        NNFloat* pOut = pbOutput->_pSysData; // data from output layer (scores)
 
-    randData(pTarget, pOut, batch, nFeatures, STRIDE);
+        randData(pTarget, pOut, batch, nFeatures, stride);
 
-    pbTarget->Upload(); // desired output
-    pbOutput->Upload(); // copy of output layer + filtering
+        pbTarget->Upload(); // desired output
+        pbOutput->Upload(); // copy of output layer + filtering
 
-    memset(pbUIValue->_pSysData, 0, batch * topK * sizeof(unsigned int));
-    pbUIValue->Upload();
-  }
-  // run test 1
-  gettimeofday(&t0, NULL);
-  kCalculateTopK(pbOutput->_pDevData, pbKey->_pDevData, pbUIValue->_pDevData, batch, STRIDE, topK);
-  gettimeofday(&t1, NULL);
-  cout << "GPU sort: " << elapsed_time(t1, t0) << endl;
+        memset(pbUIValue->_pSysData, 0, batch * topK * sizeof(unsigned int));
+        pbUIValue->Upload();
+    }
+    // run test 1
+    gettimeofday(&t0, NULL);
+    kCalculateTopK(pbOutput->_pDevData, pbKey->_pDevData, pbUIValue->_pDevData, batch, stride, topK);
+    gettimeofday(&t1, NULL);
+    cout << "GPU sort: " << elapsed_time(t1, t0) << endl;
 
-  //validate data
-  {
-    pbOutput->Download();
-    pbTarget->Download();
-    pbKey->Download();
-    pbFValue->Download();
-    pbUIValue->Download();
+    //validate data
+    {
+        pbOutput->Download();
+        pbTarget->Download();
+        pbKey->Download();
+        pbFValue->Download();
+        pbUIValue->Download();
 
-    vector<float> keys(nFeatures);
-    vector<unsigned int> topK_vals(topK);
-    vector<float> topK_keys(topK);
+        vector<float> keys(nFeatures);
+        vector<unsigned int> topKvals(topK);
+        vector<float> topKkeys(topK);
 
-    NNFloat* pOutput = pbOutput->_pSysData;
-    NNFloat* pKey = pbKey->_pSysData;
-    unsigned int* pUIValue = pbUIValue->_pSysData;
+        NNFloat* pOutput = pbOutput->_pSysData;
+        NNFloat* pKey = pbKey->_pSysData;
+        unsigned int* pUIValue = pbUIValue->_pSysData;
 
-    int countValueError = 0;
-    float sumKeyError = 0.f;
-    float cpuSort = 0.f;
+        int countValueError = 0;
+        float sumKeyError = 0.f;
+        float cpuSort = 0.f;
 
-    for (size_t i = 0; i < batch; i++) {
+        for (size_t i = 0; i < batch; i++) {
 
-      gettimeofday(&t0, NULL);
-      topKsort<NNFloat, unsigned int>(pOutput, NULL, nFeatures, &topK_keys[0], &topK_vals[0], topK);
-      gettimeofday(&t1, NULL);
-      cpuSort += elapsed_time(t1, t0);
+            gettimeofday(&t0, NULL);
+            topKsort<NNFloat, unsigned int>(pOutput, NULL, nFeatures, &topKkeys[0], &topKvals[0], topK);
+            gettimeofday(&t1, NULL);
+            cpuSort += elapsed_time(t1, t0);
 
-      for (size_t k = 0; k < topK; k++) {
-        unsigned int GPUvalue = pUIValue[k]; // index
-        float GPUkey = pKey[k]; // score
+            for (size_t k = 0; k < topK; k++) {
+                unsigned int GPUvalue = pUIValue[k]; // index
+                float GPUkey = pKey[k]; // score
 
-        float CPUvalue = topK_vals[k];
-        float CPUkey = topK_keys[k];
+                float CPUvalue = topKvals[k];
+                float CPUkey = topKkeys[k];
 
-        if (fabs(GPUvalue - CPUvalue) > EPS) {
-          countValueError++;
+                if (fabs(GPUvalue - CPUvalue) > eps) {
+                    countValueError++;
+                }
+                sumKeyError += fabs(GPUkey - CPUkey);
+            }
+            pKey += topK;
+            pUIValue += topK;
+            pOutput += stride;
         }
-        sumKeyError += fabs(GPUkey - CPUkey);
-      }
-      pKey += topK;
-      pUIValue += topK;
-      pOutput += STRIDE;
-    }
-    cout << "CPU sort: " << cpuSort << endl;
+        cout << "CPU sort: " << cpuSort << endl;
 
-    if (countValueError && sumKeyError) {
-      cout << "1 ERROR kCalculateTopK with 3 args; ";
-      ret = false;
-    } else {
-      cout << "1 PASS kCalculateTopK with 3 args; "; // some error is accepted because bitonic sort belongs to not stable sorting
-    }
-    cout << "countValueError " << countValueError << " sumKeyError " << sumKeyError << endl;
-  }
-
-  cout << "2 TEST kCalculateTopK with 4 args" << endl;
-
-  // prepare data
-  {
-    NNFloat* pTarget = pbTarget->_pSysData;
-    NNFloat* pOut = pbOutput->_pSysData; // data from output layer (scores)
-
-    randData(pTarget, pOut, batch, nFeatures, STRIDE);
-
-    pbTarget->Upload(); // desired output
-    pbOutput->Upload(); // copy of output layer + filtering
-  }
-
-  //run test
-  kCalculateTopK(pbOutput->_pDevData, pbTarget->_pDevData, pbKey->_pDevData, pbFValue->_pDevData, batch, STRIDE, topK);
-
-  //validate data
-  {
-    pbOutput->Download();
-    pbTarget->Download();
-    pbKey->Download();
-    pbFValue->Download();
-
-    vector<float> vals(nFeatures);
-    vector<float> keys(nFeatures);
-    vector<float> topK_vals(topK);
-    vector<float> topK_keys(topK);
-
-    NNFloat* pOutput = pbOutput->_pSysData;
-    NNFloat* pTarget = pbTarget->_pSysData;
-    NNFloat* pKey = pbKey->_pSysData;
-    NNFloat* pValue = pbFValue->_pSysData;
-
-    int countValueError = 0;
-    float sumKeyError = 0;
-
-    for (size_t i = 0; i < batch; i++) {
-
-      topKsort<NNFloat, NNFloat>(pOutput, pTarget, nFeatures, &topK_keys[0], &topK_vals[0], topK);
-
-      for (size_t k = 0; k < topK; k++) {
-        unsigned int GPUvalue = pValue[k]; // index
-        float GPUkey = pKey[k]; // score
-
-        float CPUvalue = topK_vals[k];
-        float CPUkey = topK_keys[k];
-
-        if (fabs(GPUvalue - CPUvalue) > EPS) {
-          countValueError++;
+        if (countValueError && sumKeyError) {
+            cout << "1 ERROR kCalculateTopK with 3 args; ";
+            ret = false;
+        } else {
+            cout << "1 PASS kCalculateTopK with 3 args; "; // some error is accepted because bitonic sort belongs to not stable sorting
         }
-        sumKeyError += fabs(GPUkey - CPUkey);
-      }
-      pKey += topK;
-      pValue += topK;
-      pOutput += STRIDE;
-      pTarget += STRIDE;
+        cout << "countValueError " << countValueError << " sumKeyError " << sumKeyError << endl;
     }
 
-    if (countValueError && sumKeyError) {
-      cout << "2 ERROR kCalculateTopK with 4 args; ";
-      ret = false;
-    } else {
-      cout << "2 PASS kCalculateTopK with 4 args; ";
+    cout << "2 TEST kCalculateTopK with 4 args" << endl;
+
+    // prepare data
+    {
+        NNFloat* pTarget = pbTarget->_pSysData;
+        NNFloat* pOut = pbOutput->_pSysData; // data from output layer (scores)
+
+        randData(pTarget, pOut, batch, nFeatures, stride);
+
+        pbTarget->Upload(); // desired output
+        pbOutput->Upload(); // copy of output layer + filtering
     }
-    cout << "countValueError " << countValueError << " sumKeyError " << sumKeyError << endl;
-  }
 
-  int totalGPUMemory;
-  int totalCPUMemory;
-  getGpu().GetMemoryUsage(&totalGPUMemory, &totalCPUMemory);
-  cout << "GPU Memory Usage: " << totalGPUMemory << " KB" << endl;
-  cout << "CPU Memory Usage: " << totalCPUMemory << " KB" << endl;
+    //run test
+    kCalculateTopK(pbOutput->_pDevData, pbTarget->_pDevData, pbKey->_pDevData, pbFValue->_pDevData, batch, stride, topK);
 
-  delete pbKey;
-  delete pbFValue;
-  delete pbTarget;
-  delete pbOutput;
-  delete pbUIValue;
+    //validate data
+    {
+        pbOutput->Download();
+        pbTarget->Download();
+        pbKey->Download();
+        pbFValue->Download();
 
-  return ret;
+        vector<float> vals(nFeatures);
+        vector<float> keys(nFeatures);
+        vector<float> topKvals(topK);
+        vector<float> topKkeys(topK);
+
+        NNFloat* pOutput = pbOutput->_pSysData;
+        NNFloat* pTarget = pbTarget->_pSysData;
+        NNFloat* pKey = pbKey->_pSysData;
+        NNFloat* pValue = pbFValue->_pSysData;
+
+        int countValueError = 0;
+        float sumKeyError = 0;
+
+        for (size_t i = 0; i < batch; i++) {
+
+            topKsort<NNFloat, NNFloat>(pOutput, pTarget, nFeatures, &topKkeys[0], &topKvals[0], topK);
+
+            for (size_t k = 0; k < topK; k++) {
+                unsigned int GPUvalue = pValue[k]; // index
+                float GPUkey = pKey[k]; // score
+
+                float CPUvalue = topKvals[k];
+                float CPUkey = topKkeys[k];
+
+                if (fabs(GPUvalue - CPUvalue) > eps) {
+                    countValueError++;
+                }
+                sumKeyError += fabs(GPUkey - CPUkey);
+            }
+            pKey += topK;
+            pValue += topK;
+            pOutput += stride;
+            pTarget += stride;
+        }
+
+        if (countValueError && sumKeyError) {
+            cout << "2 ERROR kCalculateTopK with 4 args; ";
+            ret = false;
+        } else {
+            cout << "2 PASS kCalculateTopK with 4 args; ";
+        }
+        cout << "countValueError " << countValueError << " sumKeyError " << sumKeyError << endl;
+    }
+
+    int totalGPUMemory;
+    int totalCPUMemory;
+    getGpu().GetMemoryUsage(&totalGPUMemory, &totalCPUMemory);
+    cout << "GPU Memory Usage: " << totalGPUMemory << " KB" << endl;
+    cout << "CPU Memory Usage: " << totalCPUMemory << " KB" << endl;
+
+    return ret;
 }
 
 //----------------------------------------------------------------------------
-class TestSort : public CppUnit::TestFixture
-{
-public:             // Interface
-    void            TestCPU_GPUSort()
-    {
-      {
-        const size_t BATCH = 128;
-        const size_t TOP_K = 128;
-        const size_t N_FEATURES = 1024;
-        bool result = testTopK(BATCH, TOP_K, N_FEATURES);
-        CPPUNIT_ASSERT_MESSAGE("failed with N_FEATURES = 1024, TOP_K = 128", result);
-      }
-      {
-        const size_t BATCH = 128;
-        const size_t TOP_K = 128;
-        const size_t N_FEATURES = 100000;
-        bool result = testTopK(BATCH, TOP_K, N_FEATURES);
-        CPPUNIT_ASSERT_MESSAGE("failed with N_FEATURES = 100000, TOP_K = 128", result);
-      }
-      {
-        const size_t BATCH = 128;
-        const size_t TOP_K = 64;
-        const size_t N_FEATURES = 1024;
-        bool result = testTopK(BATCH, TOP_K, N_FEATURES);
-        CPPUNIT_ASSERT_MESSAGE("failed with N_FEATURES = 1024, TOP_K = 64", result);
-      }
-      {
-        const size_t BATCH = 128;
-        const size_t TOP_K = 32;
-        const size_t N_FEATURES = 64;
-        bool result = testTopK(BATCH, TOP_K, N_FEATURES);
-        CPPUNIT_ASSERT_MESSAGE("failed with N_FEATURES = 64, TOP_K = 32", result);
-      }
-      {
-        const size_t BATCH = 128;
-        const size_t TOP_K = 1;
-        const size_t N_FEATURES = 64;
-        bool result = testTopK(BATCH, TOP_K, N_FEATURES);
-        CPPUNIT_ASSERT_MESSAGE("failed with N_FEATURES = 64, TOP_K = 32", result);
-      }
+class TestSort: public CppUnit::TestFixture {
+public:
+    // Interface
+    void TestCPU_GPUSort() {
+        // Initialize GPU
+        getGpu().SetRandomSeed(12345);
+        getGpu().CopyConstants();
+        const size_t numberTests = 5;
+        const size_t batches[numberTests] =        {128,  128,    128,  128, 128};
+        const size_t topK[numberTests] =           {128,  128,    64,   32,  1};
+        const size_t numberFeatures[numberTests] = {1024, 100000, 1024, 64,  64};
+
+        for (size_t i = 0; i < numberTests; i++) {
+            bool result = testTopK(batches[i], topK[i], numberFeatures[i]);
+            cout << "batches " << batches[i] <<  ", topK " << topK[i] << ", numberFeatures" << numberFeatures[i] << endl;
+            CPPUNIT_ASSERT_MESSAGE("failed gpuSort", result);
+        }
     }
-    
+
 public:
     CPPUNIT_TEST_SUITE(TestSort);
     CPPUNIT_TEST(TestCPU_GPUSort);
     CPPUNIT_TEST_SUITE_END();
-    
 };
