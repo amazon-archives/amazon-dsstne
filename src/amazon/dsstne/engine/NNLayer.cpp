8ccb3b7961df51ab480ce22a87cff9b61862ee6a
kiukA9 <kiuk@amazon.com>
2018-08-28 18:05:04 -0700

    Added attributes field to NNLayer, added NNLayer::GetKind.

--- src/amazon/dsstne/engine/NNLayer.cpp
+++ src/amazon/dsstne/engine/NNLayer.cpp
@@ -36,6 +36,7 @@ NNLayer::NNLayer(NNLayerDescriptor& d, uint32_t batch) :
 _name(d._name),
 _kind(d._kind),
 _type(d._type),
+_attributes(d._attributes),
 _poolingFunction(d._poolingFunction),
 _dataSet(d._dataSet),
 _pDataSet(NULL),
@@ -371,6 +372,14 @@ cudnnTensorDescriptor_t NNLayer::getTensorDescriptor(uint32_t batch)
 
 const string& NNLayer::GetName() const {
   return _name;
+}
+
+NNLayer::Kind NNLayer::GetKind() const {
+  return _kind;
+}
+
+uint32_t NNLayer::GetAttributes() const {
+  return _attributes;
 }
 
 const NNDataSetBase* NNLayer::GetDataSet() const {
