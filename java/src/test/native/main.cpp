bc424d15bf211b7aeb37c7c6292ed320f7c6298f
kiukA9 <kiuk@amazon.com>
2018-09-25 19:21:12 -0700

    Sample main.cpp in java/src/test/native.

--- java/src/test/native/main.cpp
+++ java/src/test/native/main.cpp
@@ -11,14 +11,88 @@
 #include <tuple>
 #include <mpi.h>
 
+#include "amazon/dsstne/engine/DsstneContext.h"
 #include "amazon/dsstne/engine/GpuTypes.h"
 #include "amazon/dsstne/engine/NNTypes.h"
 #include "amazon/dsstne/engine/NNLayer.h"
 #include "amazon/dsstne/engine/NNNetwork.h"
 
+using namespace std;
+
 int main(int argc, char** argv) {
-  std::cout << "sizeof(char) = " << sizeof(char) << std::endl;
-  std::cout << "sizof(uchar) = " << sizeof(unsigned char) << std::endl;
+
+    DsstneContext dc("/home/kiuk/tmp/gl.nc", 1);
+
+    NNNetwork *network = dc.getNetwork();
+
+    cout << "main.o: loaded network " << network->GetName() << endl;
+
+    vector<const NNLayer*> inputLayers;
+    vector<NNDataSetBase*> datasets;
+
+    auto it = network->GetLayers(NNLayer::Kind::Input, inputLayers);
+    for(; it != inputLayers.end(); ++it) {
+        const NNLayer *layer = (*it);
+        NNDataSetDescriptor desc;
+
+        uint32_t x,y,z,w;
+        tie(x,y,z,w) = layer->GetDimensions();
+        desc._dim = NNDataSetDimensions(x,y,z);
+        desc._name = layer->GetDataSetName();
+        desc._dataType = NNDataSetEnums::DataType::Int;
+        desc._attributes = NNDataSetEnums::None;
+        desc._examples = network->GetBatch();
+        desc._sparseDensity = 1;
+
+        NNDataSetBase *ds = createNNDataSet(desc);
+        datasets.push_back(ds);
+    }
+
+    network->LoadDataSets(datasets);
+    it = network->GetLayers(NNLayer::Kind::Input, inputLayers);
+    for(; it != inputLayers.end(); ++it) {
+        const NNLayer *layer = (*it);
+        const string &layerName = layer->GetName();
+        const string &datasetName = layer->GetDataSetName();
+        cout << "main.o: loaded " << datasetName << " to layer " << layerName << endl;
+    }
+
+    network->PredictBatch();
+    network->SetPosition(0);
+
+    const NNLayer *outputLayer = network->GetLayer("Output");
+    NNFloat *dUnitBuffer = network->GetUnitBuffer("Output");
+
+    uint32_t x,y,z,w;
+    tie(x,y,z,w) = outputLayer->GetDimensions();
+    uint32_t batchSize = network->GetBatch();
+    size_t width = x * y * z;
+    uint32_t k = 10;
+
+    NNFloat *outputScores;
+    uint32_t *outputIndexes;
+    cudaMallocManaged(&outputScores, k * batchSize * sizeof(NNFloat));
+    cudaMallocManaged(&outputIndexes, k * batchSize * sizeof(uint32_t));
+
+    kCalculateTopK(dUnitBuffer, outputScores, outputIndexes, batchSize, width, k);
+    cudaDeviceSynchronize();
+
+    for(size_t i =0; i < batchSize; ++i)
+    {
+        for(size_t j=0; j<k; ++j) {
+                printf("%u:%5.3f,", outputIndexes[i * k + j], outputScores[i*k + j]);
+            }
+    }
+
+    printf("\n");
+
+    cudaFree(outputScores);
+    cudaFree(outputIndexes);
+
+    for (const auto &d : datasets)
+    {
+        delete d;
+    }
 
 //  char *argv2 = "process";
 //  getGpu().Startup(1, &argv2);
