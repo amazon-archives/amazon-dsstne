7b774ef6cc4f0ae63316d8411879d2996ef1c634
kiukA9 <kiuk@amazon.com>
2018-10-03 14:07:34 -0700

    Added java unit tests for dsstne.predict (for sparse and dense datasets).

--- java/src/test/java/com/amazon/dsstne/NNNetworkTest.java
+++ java/src/test/java/com/amazon/dsstne/NNNetworkTest.java
@@ -17,58 +17,276 @@
 
 package com.amazon.dsstne;
 
-import java.io.IOException;
+import static org.junit.Assert.fail;
 
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
 import com.amazon.dsstne.NNDataSetEnums.DataType;
 import com.amazon.dsstne.data.DenseNNDataSet;
 import com.amazon.dsstne.data.OutputNNDataSet;
+import com.amazon.dsstne.data.SparseNNDataSet;
 
 public class NNNetworkTest {
+    private static final String DIR_SUFFIX = "src/test/java/com/amazon/dsstne/test-data/";
+    private final String networkFile = DIR_SUFFIX + "movielens_20m_autoencoder.nc";
+    private final String inputFile = DIR_SUFFIX + "movielens_input_1024.txt";
+    private final String indexFile = DIR_SUFFIX + "index.txt";
+    private final String expectedOutputFile = DIR_SUFFIX + "expected_output_1024.txt";
 
-    @Ignore
-    @Test
-    public void test() throws IOException {
-        int k = 4;
-        int batchSize = 32;
+    private final int k = 16;
+    private final int batchSize = 256;
+    private final double sparseDensity = 0.1d;
 
-        NetworkConfig config =
-            NetworkConfig.with().networkFilePath("/home/kiuk/tmp/gl.nc").batchSize(batchSize).k(k).build();
+    private NetworkConfig config;
+    private NNNetwork network;
+    private Map<String, Long> index;
+    private Map<Long, String> rIndex;
+    private List<String[]> expectedKeys;
+    private List<float[]> expectedScores;
 
-        System.out.println("Loading " + config);
+    private final float testDelta = 0.01f;
 
-        NNNetwork network = Dsstne.load(config);
-
+    @Before
+    public void setup() throws IOException {
+        config = NetworkConfig.with().networkFilePath(networkFile).batchSize(batchSize).k(k).build();
+        network = Dsstne.load(config);
         System.out.println("Loaded network: \n" + network);
 
+        index = new HashMap<>();
+        rIndex = new HashMap<>();
+        parseIndex(indexFile, index, rIndex);
+
+        expectedKeys = new ArrayList<>();
+        expectedScores = new ArrayList<>();
+        readExpectedOutput(expectedKeys, expectedScores);
+    }
+
+    @After
+    public void teardown() {
+        network.close();
+        index.clear();
+        rIndex.clear();
+    }
+
+    @Test
+    public void testPredictSparse() throws IOException {
         NNDataSet[] inputDatasets = new NNDataSet[network.getInputLayers().length];
-        for(int i=0; i<network.getInputLayers().length; ++i) {
+        for (int i = 0; i < network.getInputLayers().length; ++i) {
             NNLayer inputLayer = network.getInputLayers()[i];
-            String layerName = inputLayer.getName();
-            String datasetName = inputLayer.getDatasetName();
-            inputDatasets[i] = new DenseNNDataSet(new Dim(inputLayer.getDim(), batchSize), DataType.Int);
-            inputDatasets[i].setName(datasetName);
-            inputDatasets[i].setLayerName(layerName);
+            inputDatasets[i] =
+                new SparseNNDataSet(new Dim(inputLayer.getDim(), batchSize), DataType.Int, sparseDensity);
+            inputDatasets[i].setName(inputLayer.getDatasetName());
+            inputDatasets[i].setLayerName(inputLayer.getName());
         }
 
         network.load(inputDatasets);
-        OutputNNDataSet[] outputDatasets = network.predict(inputDatasets);
-        OutputNNDataSet output = outputDatasets[0];
-        float[] scores = output.getScores();
-        long[] indexes = output.getIndexes();
+        System.out.println("Loaded " + inputDatasets.length + " input datasets to network");
 
-        int K = (k == -1) ? 10 : k;
-        for(int i=0; i<batchSize; ++i) {
-            for(int j=0; j< K; ++j) {
-                System.out.print(String.format("%d:%5.4f,", indexes[i * K + j], scores[i * K + j]));
+        SparseDataProvider dp = new SparseDataProvider(inputFile, index);
+        assertPredictions(inputDatasets, dp);
+    }
+
+    private void assertPredictions(final NNDataSet[] inputDatasets, final DataProvider dp) throws IOException {
+        for (int batch = 0; dp.getBatch(inputDatasets[0]); ++batch) {
+            long start = System.currentTimeMillis();
+            OutputNNDataSet[] outputDatasets = network.predict(inputDatasets);
+            long end = System.currentTimeMillis();
+            System.out.println("==== Batch # " + (batch + 1) + " Predict. Took " + (end - start) + " ms ====");
+            OutputNNDataSet output = outputDatasets[0];
+            float[] scores = output.getScores();
+            long[] indexes = output.getIndexes();
+
+            for (int i = 0; i < batchSize; ++i) {
+                int pos = batch * batchSize + i;
+                String[] expectedKey = expectedKeys.get(pos);
+                float[] expectedScr = expectedScores.get(pos);
+
+                String failMessage = null;
+                for (int j = 0; j < k; ++j) {
+                    String actualKey = rIndex.get(indexes[i * k + j]);
+                    float actualScore = scores[i * k + j];
+                    if (!expectedKey[j].equals(actualKey) || Math.abs(expectedScr[j] - actualScore) > testDelta) {
+                        failMessage =
+                            String.format("Unequal key or score at input %d, k=%d. Expected: %s,%5.3f Actual: %s,%5.3f",
+                                pos + 1, j + 1, expectedKey[j], expectedScr[j], actualKey, actualScore);
+                    }
+                }
+
+                if (failMessage != null) {
+                    System.out.println("== Actual ==");
+                    System.out.print((pos + 1) + "\t");
+                    for (int j = 0; j < k; ++j) {
+                        String actualKey = rIndex.get(indexes[i * k + j]);
+                        float actualScore = scores[i * k + j];
+                        System.out.print(String.format("%s,%1.3f:", actualKey, actualScore));
+                    }
+                    System.out.println();
+                    System.out.println("== Expected ==");
+                    System.out.print((pos + 1) + "\t");
+                    for (int j = 0; j < k; ++j) {
+                        System.out.print(String.format("%s,%1.3f:", expectedKey[j], expectedScr[j]));
+                    }
+                    System.out.println();
+                    fail(failMessage);
+                }
             }
-            System.out.println();
         }
-//        for (int i = 0; i < outputDatasets.length; ++i) {
-//            outputDatasets[i].getIndexes()
-//        }
-        network.close();
+    }
+
+    @Ignore
+    @Test
+    public void testPredictDense() throws IOException {
+        NNDataSet[] inputDatasets = new NNDataSet[network.getInputLayers().length];
+        for (int i = 0; i < network.getInputLayers().length; ++i) {
+            NNLayer inputLayer = network.getInputLayers()[i];
+            inputDatasets[i] =
+                new DenseNNDataSet(new Dim(inputLayer.getDim(), batchSize), DataType.Int);
+            inputDatasets[i].setName(inputLayer.getDatasetName());
+            inputDatasets[i].setLayerName(inputLayer.getName());
+        }
+
+        network.load(inputDatasets);
+        System.out.println("Loaded " + inputDatasets.length + " input datasets to network");
+
+        DenseDataProvider dp = new DenseDataProvider(inputDatasets[0].getStride(), inputFile, index);
+
+        assertPredictions(inputDatasets, dp);
+    }
+
+    private void readExpectedOutput(List<String[]> allIndexes, List<float[]> allScores) throws IOException {
+        try (BufferedReader in = new BufferedReader(new FileReader(expectedOutputFile))) {
+            String line;
+            while ((line = in.readLine()) != null) {
+                String[] kv = line.split("\t");
+                int pos = Integer.parseInt(kv[0]);
+                String[] vs = kv[1].split(":");
+                String[] idx = new String[vs.length];
+                float[] scores = new float[vs.length];
+                for (int i = 0; i < vs.length; ++i) {
+                    String v = vs[i];
+                    idx[i] = v.split(",")[0];
+                    scores[i] = Float.parseFloat(v.split(",")[1]);
+                }
+                // pos is 1 indexed
+                allScores.add(pos - 1, scores);
+                allIndexes.add(pos - 1, idx);
+            }
+        }
+    }
+
+    private void parseIndex(final String indexFile, final Map<String, Long> index,
+        final Map<Long, String> rIndex)
+        throws IOException {
+        try (BufferedReader in = new BufferedReader(new FileReader(indexFile))) {
+            String line;
+            while ((line = in.readLine()) != null) {
+                String[] split = line.split("\t");
+                String key = split[0];
+                long idx = Long.parseLong(split[1]);
+                if (index.put(key, idx) != null) {
+                    throw new IllegalArgumentException("Duplicate key found: " + key);
+                }
+                if (rIndex.put(idx, key) != null) {
+                    throw new IllegalArgumentException("Duplicate index found: " + idx);
+                }
+            }
+        }
+    }
+
+    static abstract class DataProvider implements Closeable {
+        final Map<String, Long> indexMap;
+        final File inputFile;
+        final BufferedReader input;
+
+        DataProvider(final String inputFile, final Map<String, Long> indexMap) throws IOException {
+            this.indexMap = indexMap;
+            this.inputFile = new File(inputFile);
+            this.input = new BufferedReader(new FileReader(inputFile));
+        }
+
+        boolean getBatch(final NNDataSet dataset) throws IOException {
+            int numExamples = dataset.getExamples();
+
+            boolean eof = false;
+            for (int i = 0; i < numExamples; ++i) {
+                String line = input.readLine();
+
+                if (line == null && i == 0) {
+                    eof = true;
+                    break;
+                } else if (line != null) {
+                    String split[] = line.split("\t")[1].split(":");
+                    int[] data = new int[split.length];
+                    long[] index = new long[split.length];
+
+                    for (int j = 0; j < split.length; ++j) {
+                        String key = split[j].split(",")[0];
+                        Long idx = indexMap.get(key);
+
+                        if (idx == null) {
+                            throw new RuntimeException("No index found for key: " + key);
+                        }
+
+                        data[j] = 1;
+                        index[j] = idx;
+                    }
+                    addExample(dataset, i, index, data);
+                }
+
+            }
+            return !eof;
+        }
+
+        protected abstract void addExample(NNDataSet dataset, int idx, final long[] index, final int[] data);
+
+        @Override
+        public void close() throws IOException {
+            input.close();
+        }
+    }
+
+    static class DenseDataProvider extends DataProvider {
+        private final int[] dataBuffer;
+
+        DenseDataProvider(final int stride, final String inputFile, final Map<String, Long> indexMap)
+            throws IOException {
+            super(inputFile, indexMap);
+            this.dataBuffer = new int[stride];
+        }
+
+        @Override
+        protected void addExample(final NNDataSet dataset, final int idx, final long[] index, final int[] data) {
+            Arrays.fill(dataBuffer, 0); // clear the buffer
+            for (int i = 0; i < index.length; ++i) {
+                dataBuffer[(int) index[i]] = data[i];
+            }
+            dataset.add(idx, dataBuffer);
+        }
+    }
+
+    static class SparseDataProvider extends DataProvider {
+        SparseDataProvider(final String inputFile, final Map<String, Long> indexMap) throws IOException {
+            super(inputFile, indexMap);
+        }
+
+        @Override
+        protected void addExample(final NNDataSet dataset, final int idx, final long[] index, final int[] data) {
+            dataset.addSparse(idx, index, data);
+        }
+
     }
 }
--- java/src/test/native/main.cpp
+++ java/src/test/native/main.cpp
@@ -1,8 +1,18 @@
 /*
- * main.cpp
+ *  Copyright 2016  Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
- *  Created on: Aug 15, 2018
- *      Author: kiuk
+ *  Licensed under the Apache License, Version 2.0 (the "License").
+ *  You may not use this file except in compliance with the License.
+ *  A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0/
+ *
+ *  or in the "license" file accompanying this file.
+ *  This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ *  either express or implied.
+ *
+ *  See the License for the specific language governing permissions and limitations under the License.
+ *
  */
 #include <iostream>
 #include <utility>
@@ -10,6 +20,8 @@
 #include <map>
 #include <tuple>
 #include <mpi.h>
+#include <fstream>
+#include <sstream>
 
 #include "amazon/dsstne/engine/DsstneContext.h"
 #include "amazon/dsstne/engine/GpuTypes.h"
@@ -19,96 +31,161 @@
 
 using namespace std;
 
-int main(int argc, char** argv) {
+void readIndexFile(const string &indexFile, map<string, uint32_t> &indexMap, map<uint32_t, string> &rIndexMap)
+{
+    ifstream in(indexFile);
+    if(!in) {
+        throw runtime_error(indexFile + " does not exist");
+    }
+    string line;
+    while (getline(in, line))
+    {
+        int idx = line.find_first_of('\t');
+        string key = line.substr(0, idx);
+        uint32_t index = stoul(line.substr(idx + 1, line.size()));
+        indexMap[key] = index;
+        rIndexMap[index] = key;
+    }
+    in.close();
+}
 
-    DsstneContext dc("/home/kiuk/tmp/gl.nc", 1);
+void readInputFile(const string &inputFile, const map<string, uint32_t> &indexMap, uint64_t *sparseStart,
+                   uint64_t *sparseEnd, uint32_t *sparseIndex, int *sparseData)
+{
+    ifstream in(inputFile);
+    if(!in) {
+        throw runtime_error(inputFile + " does not exist");
+    }
+
+    string line;
+    int i = 0;
+
+    sparseStart[0] = 0;
+    while (getline(in, line))
+    {
+        if (i != 0)
+        {
+            sparseStart[i] = sparseEnd[i - 1];
+        }
+
+        int idx = line.find_first_of('\t');
+        string v = line.substr(idx + 1, line.size());
+
+        int j = 0;
+        stringstream vs(v);
+        string elem;
+        while (getline(vs, elem, ':'))
+        {
+            int vidx = elem.find_first_of(',');
+            string key = elem.substr(0, vidx);
+
+            sparseIndex[sparseStart[i] + j] = indexMap.at(key);
+            sparseData[sparseStart[i] + j] = 1;
+            ++j;
+        }
+        sparseEnd[i] = sparseStart[i] + j;
+        ++i;
+    }
+    in.close();
+}
+
+int main(int argc, char** argv)
+{
+    uint32_t k = 10;
+    uint32_t batchSize = 32;
+    float sparseDensity = 0.09;
+
+    const string networkFile = argv[1];
+    const string indexFile = argv[2];
+    const string inputFile = argv[3];
+
+    DsstneContext dc(networkFile, batchSize);
 
     NNNetwork *network = dc.getNetwork();
-
     cout << "main.o: loaded network " << network->GetName() << endl;
 
     vector<const NNLayer*> inputLayers;
-    vector<NNDataSetBase*> datasets;
+    vector<NNDataSetDescriptor> datasets;
 
     auto it = network->GetLayers(NNLayer::Kind::Input, inputLayers);
-    for(; it != inputLayers.end(); ++it) {
+    for (; it != inputLayers.end(); ++it)
+    {
         const NNLayer *layer = (*it);
         NNDataSetDescriptor desc;
 
-        uint32_t x,y,z,w;
-        tie(x,y,z,w) = layer->GetDimensions();
-        desc._dim = NNDataSetDimensions(x,y,z);
+        uint32_t x, y, z, w;
+        tie(x, y, z, w) = layer->GetDimensions();
+        desc._dim = NNDataSetDimensions(x, y, z);
         desc._name = layer->GetDataSetName();
         desc._dataType = NNDataSetEnums::DataType::Int;
-        desc._attributes = NNDataSetEnums::None;
+        desc._attributes = NNDataSetEnums::Attributes::Sparse;
         desc._examples = network->GetBatch();
-        desc._sparseDensity = 1;
+        desc._sparseDensity = sparseDensity;
 
-        NNDataSetBase *ds = createNNDataSet(desc);
-        datasets.push_back(ds);
+        datasets.push_back(desc);
     }
 
-    network->LoadDataSets(datasets);
-    it = network->GetLayers(NNLayer::Kind::Input, inputLayers);
-    for(; it != inputLayers.end(); ++it) {
-        const NNLayer *layer = (*it);
-        const string &layerName = layer->GetName();
-        const string &datasetName = layer->GetDataSetName();
-        cout << "main.o: loaded " << datasetName << " to layer " << layerName << endl;
-    }
+    dc.initInputLayerDataSets(datasets);
 
-    network->PredictBatch();
-    network->SetPosition(0);
+    map<string, uint32_t> indexes;
+    map<uint32_t, string> rIndexes;
+    readIndexFile(indexFile, indexes, rIndexes);
+    cout << "Read " << indexes.size() << " indexes" << endl;
 
-    const NNLayer *outputLayer = network->GetLayer("Output");
-    NNFloat *dUnitBuffer = network->GetUnitBuffer("Output");
-
-    uint32_t x,y,z,w;
-    tie(x,y,z,w) = outputLayer->GetDimensions();
-    uint32_t batchSize = network->GetBatch();
-    size_t width = x * y * z;
-    uint32_t k = 10;
+    const NNLayer* inputLayer = network->GetLayer("Input");
+    uint32_t x, y, z, w;
+    tie(x, y, z, w) = inputLayer->GetDimensions();
+    size_t sparseDataLength = ((float) (x * y * z * batchSize)) * sparseDensity;
+    NNDataSetBase* inputDataset = inputLayer->GetDataSet();
 
     NNFloat *outputScores;
     uint32_t *outputIndexes;
     cudaMallocManaged(&outputScores, k * batchSize * sizeof(NNFloat));
     cudaMallocManaged(&outputIndexes, k * batchSize * sizeof(uint32_t));
 
-    kCalculateTopK(dUnitBuffer, outputScores, outputIndexes, batchSize, width, k);
-    cudaDeviceSynchronize();
-
-    for(size_t i =0; i < batchSize; ++i)
+    for (int i = 0; i < 1; ++i)
     {
-        for(size_t j=0; j<k; ++j) {
-                printf("%u:%5.3f,", outputIndexes[i * k + j], outputScores[i*k + j]);
-            }
-    }
+        uint64_t *sparseStart = (uint64_t*) calloc(batchSize, sizeof(uint64_t));
+        uint64_t *sparseEnd = (uint64_t*) calloc(batchSize, sizeof(uint64_t));
+        uint32_t *sparseIndex = (uint32_t*) calloc(sparseDataLength, sizeof(uint32_t));
+        int *sparseData = (int*) calloc(sparseDataLength, sizeof(int));
 
-    printf("\n");
+        readInputFile(inputFile, indexes, sparseStart, sparseEnd, sparseIndex, sparseData);
+
+        inputDataset->LoadSparseData(sparseStart, sparseEnd, sparseData, sparseIndex);
+
+        free(sparseStart);
+        free(sparseEnd);
+        free(sparseIndex);
+        free(sparseData);
+
+        network->SetPosition(0);
+        network->PredictBatch();
+
+        const NNLayer *outputLayer = network->GetLayer("Output");
+        NNFloat *dUnitBuffer = network->GetUnitBuffer("Output");
+
+        tie(x, y, z, w) = outputLayer->GetDimensions();
+
+        size_t width = x * y * z;
+
+        kCalculateTopK(dUnitBuffer, outputScores, outputIndexes, batchSize, width, k);
+        cudaDeviceSynchronize();
+
+        for (size_t i = 0; i < batchSize; ++i)
+        {
+            printf("%d\t", i+1);
+            for (size_t j = 0; j < k; ++j)
+            {
+
+                const string &idx = rIndexes.at(outputIndexes[i * k + j]);
+                printf("%s:%5.3f,", idx.c_str(), outputScores[i * k + j]);
+            }
+            printf("\n");
+        }
+
+    }
 
     cudaFree(outputScores);
     cudaFree(outputIndexes);
-
-    for (const auto &d : datasets)
-    {
-        delete d;
-    }
-
-//  char *argv2 = "process";
-//  getGpu().Startup(1, &argv2);
-//  MPI_Init(&argc, &argv);
-
-//  int _numprocs;
-//  int _id;
-//  MPI_Comm_size(MPI_COMM_WORLD, &_numprocs);
-//  MPI_Comm_rank(MPI_COMM_WORLD, &_id);
-
-//  std::cout << "initialized MPI " << _numprocs << ", " << _id << std::endl;
-
-//  getGpu().Shutdown();
-// getGpu().Startup(argc, argv);
-// NNNetwork *network = LoadNeuralNetworkNetCDF(argv[1], 32);
-// std::vector<const NNLayer*> inputLayers = network->GetLayers(NNLayer::Kind::Input);
-// std::cout << "Input Layer Size: " << inputLayers.size() << std::endl;
-// getGpu().Shutdown();
 }
